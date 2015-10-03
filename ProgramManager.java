6b3e41184e72b72f6b6f91dd51903a7215967d92
snuss967 <spencer.nussrallah@gmail.com>
2015-10-02 19:15:53 -0500

    Assignment 1 part 3 of 3

---
+++ ProgramManager.java
@@ -0,0 +1,10 @@
+
+public class ProgramManager {
+	//runs the program the OS
+	public static void main(String[] args)
+	{
+		AccountUI account = new AccountUI();
+		account.Welcome();
+		account.UserDialog();
+	}
+}
